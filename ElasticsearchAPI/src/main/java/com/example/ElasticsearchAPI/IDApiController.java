c2ec06ae69f00f8a430bd4baab27bce061097c93
oofy <oofysotry@gmail.com>
2024-07-23 15:20:01 +0900

    id와 title 검색 분리

---
+++ ElasticsearchAPI/src/main/java/com/example/ElasticsearchAPI/IDApiController.java
@@ -0,0 +1,53 @@
+package com.example.ElasticsearchAPI;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.ElasticsearchItemDTO.Item;
+import org.json.JSONObject;
+import java.io.IOException;
+
+@RestController
+public class IDApiController {
+    RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+    ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+    ElasticsearchClient client = new ElasticsearchClient(transport);
+
+    @GetMapping(value = "/id_item")
+    public @ResponseBody ResponseEntity<String> search(@RequestParam(name = "id") String id) throws IOException, JSONException {
+        String title = "";
+        String body = "";
+
+        SearchResponse<Item> search = client.search(s -> s
+                        .index("items")
+                        .query(q -> q
+                                .term(t -> t
+                                        .field("_id")
+                                        .value(v -> v.stringValue(id))
+                                )),
+                Item.class);
+
+        for (Hit<Item> hit: search.hits().hits()) {
+            title = hit.source().getTitle();
+            body = hit.source().getBody();
+        }
+
+        JSONObject msg = new JSONObject();
+        msg.put("title", title);
+        msg.put("body", body);
+
+        return new ResponseEntity<>(msg.toString(), HttpStatus.OK);
+    }
+}
--- ElasticsearchAPI/src/main/java/com/example/Elasticsearchcontroller/IdSearchController.java
+++ ElasticsearchAPI/src/main/java/com/example/Elasticsearchcontroller/IdSearchController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SearchController {
+public class IdSearchController {
 
-    @GetMapping("/search")
+    @GetMapping("/idesearch")
     public String index() {
-        return "search";
+        return "idsearch";
     }
 }
